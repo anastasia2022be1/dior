17f44b67f60e85e3425f6f9fb850895d7699e669
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2025-01-06 11:08:26 +0100

    section about- slider about

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-const swiper = new Swiper('.swiper', {
+const swiperTop = new Swiper('.top__swiper', {
     // Optional parameters
     effect: 'fade',
     autoplay: {
         delay: 3500,
         disableOnInteraction: false
     },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+const swiperAbout = new Swiper(".about__slider", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    freeMode: true,
 
     // Navigation arrows
     navigation: {
